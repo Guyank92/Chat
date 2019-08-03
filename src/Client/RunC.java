45807c33899c10e70fa5f4be5eba248968c15c58
Guyank92 <44778258+Guyank92@users.noreply.github.com>
2019-08-03 20:17:07 +0300

    Add files via upload

---
+++ src/Client/RunC.java
@@ -0,0 +1,13 @@
+package Client;
+
+
+/**
+ * Main file to execute the program - Client Chat
+ */
+public class RunC {
+
+    public static void main(String[] args) {
+        new ClientGUI();
+        
+    }
+}
---
+++ src/Server/Run.java
@@ -0,0 +1,11 @@
+package Server;
+
+/**
+ * Main file to execute the program - Server chat *
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        new ServerGUI();
+    }
+}
